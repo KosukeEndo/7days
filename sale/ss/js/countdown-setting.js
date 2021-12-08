64a057b8d5b20205a92c84bf79be23b0d44c27ee
email <contact@bridgez.jp>
2021-12-08 22:15:46 +0900

    最初のプッシュ

---
+++ sale/ss/js/countdown-setting.js
@@ -0,0 +1,24 @@
+// JavaScript Document
+
+
+  // イベント 開始～終了時間
+  // 開始時間
+  $('.timermarathonst').yycountdown({ endDateTime: '2021/09/04 20:00:00' });// イベント開始時間を記入
+  // 終了時間
+  $('.timermarathoned').yycountdown({ endDateTime: '2021/09/11 01:59:59' });// イベント終了時間を記入
+
+  // 日替わりタイムバーゲン
+  $('.210901tb').yycountdown({　endDateTime: '2021/09/02 09:59:59' });
+  $('.210902tb').yycountdown({　endDateTime: '2021/09/03 09:59:59' });
+  $('.210903tb').yycountdown({　endDateTime: '2021/09/04 09:59:59' });
+  $('.210904tb').yycountdown({　endDateTime: '2021/09/05 09:59:59' });
+  $('.210905tb').yycountdown({　endDateTime: '2021/09/06 09:59:59' });
+  $('.210906tb').yycountdown({　endDateTime: '2021/09/07 09:59:59' });
+  $('.210907tb').yycountdown({　endDateTime: '2021/09/08 09:59:59' });
+  $('.210908tb').yycountdown({　endDateTime: '2021/09/09 09:59:59' });
+  $('.210909tb').yycountdown({　endDateTime: '2021/09/10 09:59:59' });
+  $('.210910tb').yycountdown({　endDateTime: '2021/09/11 09:59:59' });
+  $('.210911tb').yycountdown({　endDateTime: '2021/09/12 09:59:59' });
+  $('.210912tb').yycountdown({　endDateTime: '2021/09/13 09:59:59' });
+  $('.210913tb').yycountdown({　endDateTime: '2021/09/14 09:59:59' });
+  $('.210914tb').yycountdown({　endDateTime: '2021/09/15 09:59:59' });
